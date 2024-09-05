9ea2e23ea63695d582f36ff21815479bbcfa456b
Avantika tiwari <avantikatiwari18@gmail.com>
2024-09-06 01:23:39 +0530

    practise

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,48 +1,57 @@
-
-import Axios from 'axios';
-import { useState } from 'react';
+import Axios from "axios";
+import { useState } from "react";
 import { GiLoveSong } from "react-icons/gi";
 import { FcSearch } from "react-icons/fc";
 
 function Main() {
-    const [artist, setArtist] = useState("");
-    const [song, setSong] = useState("");
-    const [lyrics, setLyrics] = useState("");
+  const [artist, setArtist] = useState("");
+  const [song, setSong] = useState("");
+  const [lyrics, setLyrics] = useState("");
 
-    function searchLyrics() {
-        if (artist === "" || song === "") {
-            return;
-        }
-        Axios.get(
-`https://api.lyrics.ovh/v1/${artist}/${song}`).then(res => {
-            console.log(res.data.lyrics);
-            setLyrics(res.data.lyrics);
-        })
+  function searchLyrics() {
+    if (artist === "" || song === "") {
+      return;
     }
+    Axios.get(`https://api.lyrics.ovh/v1/${artist}/${song}`).then((res) => {
+      console.log(res.data.lyrics);
+      setLyrics(res.data.lyrics);
+    });
+  }
 
-    
+  return (
+    <div className="App">
+      <h1>
+        Lyrics Finder <GiLoveSong />{" "}
+      </h1>
 
-    return (
-        <div className="App">
-            <h1>Lyrics Finder <GiLoveSong /> </h1>
+      <input
+        className="inp"
+        type="text"
+        placeholder="Artist name"
+        onChange={(e) => {
+          setArtist(e.target.value);
+        }}
+      />
+      <input
+        className="inp"
+        type="text"
+        placeholder="Song name"
+        onChange={(e) => {
+          setSong(e.target.value);
+        }}
+      />
+      <button className="btn" onClick={() => searchLyrics()}>
+        Search <FcSearch />{" "}
+      </button>
+      <hr />
+      <div>
+      <h1>Lyrics of Your Favourite Song</h1>
 
-            <input className="inp" type="text" 
-                placeholder='Artist name'
-                onChange={(e) => { setArtist(e.target.value) }} 
-                 />
-            <input className="inp" type="text" 
-                placeholder='Song name'
-                onChange={(e) => { setSong(e.target.value) }}
-                 />
-            <button className="btn" 
-            onClick={() => searchLyrics()}
-                >
-                     Search <FcSearch /> </button>
-            <hr />
-            <pre>{lyrics}</pre>
-        
-        </div>
-    );
+      <pre>{lyrics}</pre>
+      </div>
+      
+    </div>
+  );
 }
 
 export default Main;
